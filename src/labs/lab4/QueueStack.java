e63b015822ad1ae10d5a0ef08acc07818d310a17
Palpeleno <60727944+Palpeleno@users.noreply.github.com>
2022-02-02 15:06:10 -0500

    start of lab 4 make a class QueueStack just like Stack make methods that interact with the data push pop peek isEmpty

---
+++ src/labs/lab4/QueueStack.java
@@ -0,0 +1,7 @@
+package labs.lab4;
+
+public class QueueStack {
+
+
+    
+}
---
+++ src/labs/lab4/main.java
@@ -0,0 +1,8 @@
+package labs.lab4;
+
+public class main {
+    public static void main(String[] args) {
+        
+    }
+    
+}

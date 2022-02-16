a76be4c57c2ca4ebf3fea661705b12b671a1ab2e
Kush Darji <kdarji1@student.gsu.edu>
2022-02-15 19:17:33 -0500

    fin lab 5own hashtable

--- src/labs/lab5/main.java
+++ src/labs/lab5/main.java
@@ -1,21 +1,19 @@
 package labs.lab5;
 
 public class main {
-    {
-        HashTable<String, Integer> hash = new HashTable<>();
-        hash.put("this", 1);
-        hash.put("coder", 2);
-        hash.put("this", 4);
-        hash.put("hi", 5);
-        System.out.println(hash.size());
-        System.out.println(hash.remove("this"));
-        System.out.println(hash.remove("this"));
-        System.out.println(hash.size());
-        System.out.println(hash.isEmpty());
+    
+        public static void main(String[] args) {
+            
+            HashTable<String, Integer> hash = new HashTable<>();
+            hash.put("this", 1);
+            hash.put("coder", 2);
+            hash.put("this", 4);
+            hash.put("hi", 5);
+            System.out.println(hash.size());
+            System.out.println(hash.remove("this"));
+            System.out.println(hash.remove("this"));
+            System.out.println(hash.size());
+            System.out.println(hash.isEmpty());
+        }
     }
 
-
-
-
-        
-}

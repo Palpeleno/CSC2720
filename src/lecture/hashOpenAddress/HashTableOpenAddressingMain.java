43f9e6b9b0424fcf09442da8c60b10a8f75f903c
Palpeleno <60727944+Palpeleno@users.noreply.github.com>
2022-02-16 18:59:57 -0500

    lab question ith hash tabeles and stacks solution & linked list

---
+++ src/lecture/hashOpenAddress/HashTableOpenAddressingMain.java
@@ -0,0 +1,40 @@
+package lecture.hashOpenAddress;
+
+// public class HashTableOpenAddressingMain {
+
+// 	public static void main(String[] args) {
+// //		HashTableOpenAddressing hash = new HashTableOpenAddressing(5);
+// 		HashTableOpenAddressingRefractored hash = new HashTableOpenAddressingRefractored(5);
+// 		hash.put(1, "A");
+// 		hash.put(3, "B");
+// 		System.out.println(hash);
+// 		System.out.println(hash.size());
+// 		hash.put(6, "C");
+// 		hash.put(11, "D");
+// 		System.out.println(hash);
+// 		System.out.println(hash.size());
+// 		hash.put(11, "A");
+// 		hash.put(16, "E");
+// 		hash.put(16, "E");
+// 		System.out.println(hash);
+// 		System.out.println(hash.size());
+// 		System.out.println(hash.get(3));
+// 		hash.put(16, "F");
+// 		System.out.println(hash);
+// 		System.out.println(hash.size());
+// 		hash.remove(16);		
+// 		System.out.println(hash);
+// 		System.out.println(hash.size());
+// 		hash.remove(1);
+// 		System.out.println(hash);
+// 		System.out.println(hash.size());
+// 		System.out.println(hash.get(1));
+// 		hash.put(31, "W");
+// 		hash.put(41, "Y");
+// 		System.out.println(hash);
+// 		hash.put(41, "Z");
+// 		System.out.println(hash);
+// 		System.out.println(hash.size());
+// 	}
+
+// }

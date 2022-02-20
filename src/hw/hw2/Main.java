21702255c5d10d6cb77c635b8dd8c2a6bdda7fa6
Palpeleno <60727944+Palpeleno@users.noreply.github.com>
2022-02-19 22:20:15 -0500

    start of hw 2

---
+++ src/hw/hw2/Main.java
@@ -0,0 +1,28 @@
+package hw.hw2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] array1 = { 1, 2, 5, 2, 2, 2, 8, 2, 100, 13, 2, 9, 2, 2, 2, 7 };
+		System.out.println(majorityElement(array1));
+		int[] array2 = { 1, 4, 10, 13, 15 };
+		System.out.println(pairsWithDifference(3, array2));
+		int[] array3 = { 2, 3, 4, -8, 1, -2, 8, 7 };
+		System.out.println(containsSubarrayWithSum(5, array3));
+	}
+
+	private static char[] containsSubarrayWithSum(int i, int[] array3) {
+		return null;
+	}
+
+	private static Object pairsWithDifference(int i, int[] array2) {
+		return null;
+	}
+
+	// takes array puts in to hash map then makes the elemnts the key and counts
+	// each element and assigns the value to the key if the element shows up in the
+	// key. if it does update by adding 1
+	private static char[] majorityElement(int[] array1) {
+		return null;
+	}
+}

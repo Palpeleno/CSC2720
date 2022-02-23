e03d6fa1b524ffbec87853fd86b4ab0ae3ccac47
Kush Darji <kdarji1@student.gsu.edu>
2022-02-22 20:22:21 -0500

    finished hw 2 p 3 in O(n)

--- src/hw/hw2/Main.java
+++ src/hw/hw2/Main.java
@@ -19,14 +19,65 @@ public class Main {
 
 		System.out.println("\n");
 
-		int[] array3 = { 2, 3, 4, -8, 1, -2, 8, 7 };
-		System.out.println(containsSubarrayWithSum(5, array3));
+		int[] array3 = { 1, 1, 1, 1, 1, 2, 3, 4, -8, 1, -2, 8, 7 };
+		System.out.println(containsSubarrayWithSum(3, array3));
 	}
 
+	// this is method 3
 
-	private static boolean containsSubarrayWithSum(int i, int[] array3) {
-		
-	return true;
+	// what we going to do is go thorught the array and only add it to hash map for
+	// every element. for every element i want to update the currentsum added to teh
+	// value at the index i
+	// and then check if the currentsum- the desired sum = 0
+	// if it equal zero that mean it evens out and all the elements prior helped get
+	// that will have been added to the hashmap, the hash map will only include the
+	// first set of elements that help get the sum.
+	// true if array has a set in which equal the sum
+	// false otherwise
+	//
+	// { 2, 3, 4, -8, 1, -2, 8, 7 } sum is 5 in this case
+	//
+	private static boolean containsSubarrayWithSum(int sum, int[] array3) {
+		int current_sum = 0;
+		int start = 0;
+		int end = -1;
+
+		HashMap<Integer, Integer> tikka = new HashMap<>();
+
+		for (int i = 0; i < array3.length; i++) {
+			// System.out.println("current_sum=" + current_sum); //debug element
+			current_sum = current_sum + array3[i];
+			// System.out.println("@" + i + "=" + array3[i]); // debug element
+			// System.out.println("current sum=" + current_sum); // debug element
+
+			// check whether current_sum - sum = 0, if 0 it means
+			// the sub array is starting from index 0- so stop
+			if (current_sum - sum == 0) {
+				start = 0;
+				end = i;
+				break;
+			}
+			// if hashMap already has the value, means we already
+			// have subarray with the sum - so stop
+			if (tikka.containsKey(current_sum - sum)) {
+				start = tikka.get(current_sum - sum) + 1;
+				end = i;
+				break;
+			}
+			// if value is not present then add to hashmap
+			tikka.put(current_sum, i);
+			// System.out.println(tikka); //debug element
+
+		}
+		// if end is -1 : means we have reached end without the sum
+		if (end == -1) {
+			System.out.println("No subarray with given sum exists");
+			return false;
+		} else {
+			System.out.println("Sum found between indexes "
+					+ start + " to " + end);
+			return true;
+		}
 	}
 
 	// this is method 2
@@ -82,10 +133,10 @@ public class Main {
 		HashMap<Integer, Integer> panipuri = new HashMap<>();
 
 		for (int i = 0; i < array1.length; i++) { // traverse through array
-			//System.out.println("i=" + i); // debug element
+			// System.out.println("i=" + i); // debug element
 
 			if (panipuri.containsKey(array1[i])) { // if element exist update counter and moves to next element,
-				//System.out.println(panipuri); // debug element
+				// System.out.println(panipuri); // debug element
 				int count = panipuri.get(array1[i]) + 1;
 				if (panipuri.containsValue((array1.length / 2))) { // if elem exist and more than n/2 print and exit
 					System.out.println("majority element: " + array1[i] + '\n' + "frequency");
@@ -95,9 +146,9 @@ public class Main {
 
 			} else
 				panipuri.put(array1[i], 1); // if elem not exist add to hash
-			//System.out.println(panipuri); // debug element
+			// System.out.println(panipuri); // debug element
 		}
-		//System.out.println(panipuri); // deubg element
+		// System.out.println(panipuri); // deubg element
 		return -1; // if loop ends and makes it here that means there is no majorityelement
 	}
 
